6f8d7183d29488d756ffe723e7f72e9c02b02367
camilovegag <camilovegag@unisabana.edu.co>
2020-11-18 16:26:39 -0500

    Add onClick events to open social media 🚀

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,14 +3,12 @@ import styles from "./index.module.css";
 import { MailOutlined, PhoneOutlined, PushpinOutlined, FacebookOutlined, InstagramOutlined, YoutubeOutlined } from "@ant-design/icons";
 
 const Footer = () => {
-  const logo = "https://firebasestorage.googleapis.com/v0/b/claudio-monteverdi.appspot.com/o/logos%2Flogo.svg?alt=media&token=6a6abe39-bb52-45c9-a65a-49d6b953af9b";
   const privacy = "https://firebasestorage.googleapis.com/v0/b/claudio-monteverdi.appspot.com/o/docs%2FPolitica%20de%20Privacidad.pdf?alt=media&token=4acdee36-1297-4e74-bfc8-d0dfd39ea9aa";
+  const facebook = "https://www.facebook.com/Gimnasio-Claudio-Monteverdi-651313651594045/";
+  const instagram = "https://www.instagram.com/gcmonteverdi/";
+  const youtube = "https://www.youtube.com/channel/UC0yPJlGKEnBEitv0yZIMtIg";
   return (
     <footer>
-      {/* <div className={styles["logo"]}>
-        <img src={logo} alt="" />
-        <h1 className={styles["logo-name"]}>Gimnasio <br /> Claudio <br /> Monteverdi</h1>
-      </div> */}
       <div className={styles["info"]}>
         <p>¿Cómo nos puedes contactar?</p>
         <div className={styles["email"]}>
@@ -30,21 +28,21 @@ const Footer = () => {
         <p>Síguenos en nuestras redes sociales</p>
         <div className={styles["facebook"]}>
           <FacebookOutlined />
-          <p>Facebook</p>
+          <p onClick={() => window.open(facebook)}>Facebook</p>
         </div>
         <div className={styles["instagram"]}>
           <InstagramOutlined />
-          <p>Instagram</p>
+          <p onClick={() => window.open(instagram)}>Instagram</p>
         </div>
         <div className={styles["youtube"]}>
           <YoutubeOutlined />
-          <p>Youtube</p>
+          <p onClick={() => window.open(youtube)}>Youtube</p>
         </div>
       </div>
       <div className={styles["docs"]}>
         <p>Documentación</p>
         <div className={styles["privacy-policy"]}>
-          <p onClick={() => window.open({ privacy })}>Políticas de privacidad</p>
+          <p onClick={() => window.open(privacy)}>Políticas de privacidad</p>
         </div>
       </div>
     </footer>
