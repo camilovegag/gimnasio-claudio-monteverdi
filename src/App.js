b1f10a2471c9d0c6e41331de43d1bfeda37531d6
camilovegag <camilovegag@unisabana.edu.co>
2020-11-21 13:15:46 -0500

    Restructure about page files 📏

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import About from "./components/About";
 import Footer from "./components/Footer";
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
       </Switch>
       <Footer />
     </Router>
